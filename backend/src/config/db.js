329ee1e3ca7fea5a1ee51400ad49299f167b3755
ninu maria james <ninumariajames@gmail.com>
2026-06-05 16:58:45 +0530

    Fix MySQL hostname

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'mysql',
+    host: 'learnnest-mysql',
     user: 'root',
     password: 'root123',
     database: 'learnnest'
