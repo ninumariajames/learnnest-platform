4ac49428ad19db76acfedb69eec5476286bc1de2
ninu maria james <ninumariajames@gmail.com>
2026-06-08 08:04:40 +0530

    Add upload listing API

--- backend/src/routes/uploadRoutes.js
+++ backend/src/routes/uploadRoutes.js
@@ -39,4 +39,19 @@ router.post(
     }
 );
 
+router.get('/', (req, res) => {
+    db.query(
+        'SELECT * FROM uploads ORDER BY uploaded_at DESC',
+        (err, results) => {
+            if (err) {
+                return res.status(500).json({
+                    error: err.message
+                });
+            }
+
+            res.json(results);
+        }
+    );
+});
+
 module.exports = router;
